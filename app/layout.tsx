75339c3ae9fb1b86bae73fa528790c148ff1a4da
M@W <mehdicr7wahid@gmail.com>
2024-06-02 17:09:11 +0430

    added animation and text

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YDTO Org",
+  title: "YDTO.org",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+import Image from "next/image";
 import React from "react";
+import { easeOut, motion } from "framer-motion";
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <div className="">
+      <motion.nav
+        initial={{ opacity: 0, y: -40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2, ease: easeOut }}
+        className="pt-6 pl-6"
+      >
+        {/* <Image src={""} alt="" /> */}
+        <h1 className="font-bold text-[48px]">YDTO.org</h1>
+      </motion.nav>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2, duration: 1 }}
+        className="flex justify-center items-center h-[calc(100vh-150px)]  overflow-hidden "
+      >
+        <h1 className="  text-[32px] ">
+          Pardon Our Dust, We&apos;re Crafting a New Website!
+        </h1>
+      </motion.div>
+    </div>
+  );
 }
 
 export default Page;
