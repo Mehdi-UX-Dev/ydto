1261d8fadbad3c73fb74faca0afd9fae35f430d6
M@W <mehdicr7wahid@gmail.com>
2024-07-06 15:20:56 +0430

    added animation to the partners

--- components/partners.tsx
+++ components/partners.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 const pics = [
   "asara.png",
@@ -10,9 +11,38 @@ const pics = [
   "skatiestan.png",
   "startsteps.png",
 ];
-function Partners() {
+// function  Partners() {
+//   return (
+//     <div className="flex flex-col lg:flex-row mx-6">
+//       <section className="text-center lg:text-left">
+//         <h1 className=" lg:text-[2rem] font-bold lg:w-[15em]">Our Partners</h1>
+//         <p className="lg:w-[35em] lg:text-[1.5rem] max-w-prose mx-auto lg:mx-0 ">
+//           We work with and receive funding from amazing partners across
+//           government, philanthropy, business, and technology.
+//         </p>
+//       </section>
+
+//       <div className="flex  flex-wrap ">
+//         {pics.map((image) => (
+//           <Image
+//             key={image}
+//             src={`/assets/${image}`}
+//             alt="Innovation"
+//             height={200}
+//             width={200}
+//             className=" max-w-[500px] hidden lg:block object-contain"
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Partners;
+
+const Slider = () => {
   return (
-    <div className="flex flex-col lg:flex-row mx-6">
+    <div className="flex">
       <section className="text-center lg:text-left">
         <h1 className=" lg:text-[2rem] font-bold lg:w-[15em]">Our Partners</h1>
         <p className="lg:w-[35em] lg:text-[1.5rem] max-w-prose mx-auto lg:mx-0 ">
@@ -20,21 +50,30 @@ function Partners() {
           government, philanthropy, business, and technology.
         </p>
       </section>
-
-      <div className="flex  flex-wrap ">
-        {pics.map((image) => (
-          <Image
-            key={image}
-            src={`/assets/${image}`}
-            alt="Innovation"
-            height={200}
-            width={200}
-            className=" max-w-[500px] hidden lg:block object-contain"
-          />
-        ))}
+      <div className="relative overflow-hidden w-full flex items-center">
+        <motion.div
+          className="flex whitespace-nowrap items-center"
+          animate={{ x: [-500, 1000] }}
+          transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
+        >
+          {pics.map((image, index) => (
+            <div key={index} className="flex-none px-2">
+              <Image
+                key={image}
+                src={`/assets/${image}`}
+                alt="Innovation"
+                height={200}
+                width={200}
+                className=" max-w-[500px] hidden lg:block object-contain"
+              />
+            </div>
+          ))}
+        </motion.div>
+        {/* <div className="absolute inset-y-0 left-0 w-12 pointer-events-none z-10 bg-gradient-to-r from-white to-transparent"></div>
+        <div className="absolute inset-y-0 right-0 w-12 pointer-events-none z-10 bg-gradient-to-l from-white to-transparent"></div> */}
       </div>
     </div>
   );
-}
+};
 
-export default Partners;
+export default Slider;
