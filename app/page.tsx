580a6ed3a6e755fb3a3f31ff480070599209bb3f
M@W <mehdicr7wahid@gmail.com>
2024-07-20 20:00:50 +0430

    modified partner logos

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,17 @@ function Page() {
   return (
     <>
       <Navbar />
+      <div className="absolute h-screen w-full -z-10   bg-white opacity-10 "></div>
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 2, ease: "easeInOut", delay: 2 }}
-        className=" mt-10 lg:mt-[40px]  space-y-[240px]  mb-[240px]"
+        className=" mt-10 lg:mt-[40px]  space-y-[120px] lg:space-y-[240px]  mb-[240px]"
       >
         <HeroSection />
         <Programs />
         <Info />
-        <Partners />
+        {/* <Partners /> */}
         <Team />
         <Contact />
       </motion.div>
--- components/herosection.tsx
+++ components/herosection.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import giz from "@/public/assets/giz.png";
+import wfp from "@/public/assets/wfp.png";
+import corp from "@/public/assets/corps.png";
+import mrastah from "@/public/assets/mrastah.png";
 
 function HeroSection() {
   return (
-    <div className="max-w-[80rem] lg:mx-auto   items-center mx-6 h-[75vh] flex flex-col justify-center ">
-      <section className="text-center  ">
-        <h1 className="font-bold text-[2rem] lg:text-[3rem] max-w-[25ch] mx-auto ">
+    <div className="max-w-[80rem]   lg:mx-auto  items-center mx-6 h-[75vh] flex flex-col justify-between ">
+      <section className="text-center grow flex flex-col justify-center ">
+        <h1 className="font-bold text-[2rem]  lg:text-[3rem] max-w-[25ch] mx-auto ">
           Empowering Afghanistan’s Future Leaders and Innovators
         </h1>
         {/* <h5 className="text-[2rem] ">Equipping Youth to:</h5> */}
@@ -32,6 +37,17 @@ function HeroSection() {
           </button>
         </div>
       </section>
+      <div className="flex mt-10  items-center flex-col md:flex-row ">
+        <h3 className="hidden md:block text-[1.5rem] text-gray-400">
+          Our Great Partners:
+        </h3>
+        <div className="flex overflow-hidden flex-row items-center gap-8 ">
+          <Image src={giz} className=" w-20 object-cover" alt="" />
+          <Image src={wfp} className="w-40 object-cover" alt="" />
+          <Image src={corp} className=" w-20 object-cover" alt="" />
+          <Image src={mrastah} className=" w-20 object-cover" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
