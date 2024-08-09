2567955988d73d8bca03790834d5e29575501304
M@W <mehdicr7wahid@gmail.com>
2024-08-09 16:12:05 +0430

    changed footer design

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import facebook from "@/public/assets/facebook.svg";
 import linkedin from "@/public/assets/linkedin.svg";
 import instagram from "@/public/assets/instagram.svg";
-import medium from "@/public/assets/medium.svg";
-import { Copyright, Phone } from "lucide-react";
+import { Copyright } from "lucide-react";
 import Logo from "@/public/assets/logo.svg";
 import Link from "next/link";
 
@@ -12,45 +11,50 @@ function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="px-10 py-10 mt-5  bg-gray-900">
-      <div className="flex gap-4 items-center">
+    <footer className="px-10 py-10 mt-5  bg-gray-900 text-white ">
+      <div className="flex gap-4">
         <Image src={Logo} alt="Logo" className="w-20 mb-2" />
-        <h3 className="max-w-[25ch]">
-          Youth Digital Training and Development Organization
-        </h3>
-      </div>
-      {/* //todo we need a slogan for the org */}
-      <div className="flex items-start gap-8 py-5 lg:text-[1.5rem]">
-        <div>
-          <p>About Us</p>
-          <p>Programs</p>
-          <p>Community</p>
-          <p>Stories</p>
-        </div>
-        <div>
-          <div className="flex flex-wrap text-white gap-2">
-            <Image src={facebook} alt="facebook" className="text-white" />
-            <Image src={instagram} alt="instagram" />
-            <Image src={linkedin} alt="linkedin" />
-            <Image src={medium} alt="medium" />
-          </div>
-          <div className="flex items-center gap-2">
-            <Phone className="hidden md:block" />
-            <Link href={"tel:+93744063886"} type="">
-              +93 744063886
-            </Link>
-          </div>
+        <div className="space-y-1">
+          <h3 className="max-w-[25ch]">
+            Youth Digital Training and Developement Organization
+          </h3>
+          <h3>د ځوانانو د ډیجیټل زده کړی او پرمختګ موسسه</h3>
         </div>
       </div>
-      <div className="border-b border-white"></div>
+      <div className="flex items-start gap-14 py-14 ">
+        <div>
+          <h3 className="text-[1.5rem] pb-4">Who We Are</h3>
+          <ul className="space-y-1">
+            <li>Our Leadership</li>
+            <li>Our Achievements</li>
+            <li>Our Background</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-[1.5rem] pb-4">What We Do</h3>
+          <ul className="space-y-1">
+            <li>Digital Training</li>
+            <li>Web Developement Bootcamp</li>
+            <li>UX/UI Bootcamp</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-[1.5rem] pb-4">Contact Us</h3>
+          <p className="pb-1">Adress: Taimani 13th Street</p>
+          <p className="pb-1">Show On Map</p>
+          <Link href={"tel:+93123456789"}>Tel:+93789456789</Link>
+        </div>
+      </div>
+      <div className="flex flex-wrap text-white gap-4 pb-2">
+        <Image src={facebook} alt="facebook" className="w-10" />
+        <Image src={instagram} alt="instagram" className="w-10" />
+        <Image src={linkedin} alt="linkedin" className="w-10`" />
+      </div>
       <div className="flex flex-col md:flex-row justify-between items-center pt-2 ">
-        <div className="flex gap-1 text-[1.5rem] items-center">
-          <p>ydtdo.org</p>
-          <Copyright /> <p>{year}</p>
-        </div>
-        <div className="md:flex gap-2 ">
-          <p className="underline">DISCLAIMER</p>
-          <p className="underline">PRIVACY POLICY</p>
+        <div className="flex gap-1 items-center">
+          <p>Copyright</p>
+          <Copyright />{" "}
+          <p>{year} Youth Digital Training and Developement Organization</p>
         </div>
       </div>
     </footer>
